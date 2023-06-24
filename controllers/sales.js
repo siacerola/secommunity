571a4c304d636fbc9874b393fc358749cf92599c
siacerola <si.acerola@gmail.com>
2023-06-24 09:27:01 +0700

    ferature - createSales

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,9 +1,20 @@
 const Sales = require("../model/sales")
 
 async function createSales(req, res) {
-    const salesName= req.body.salesName
+    const { salesName, noTelp } = req.body
+    
+    const newSales = new Sales()
+    newSales.salesName = salesName
+    newSales.noTelp = noTelp
+    
+    try {
+        await Sales.create(newSales)
+    } catch (error) {
+        console.log(error);
+    }
+    res.status(200).json(newSales)
 }
 
 module.exports = {
-    
+    createSales:createSales
 }
